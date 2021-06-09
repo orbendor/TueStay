d72911c986aae1702a9d64845b4a818a87c3613b
orbendor <73276306+orbendor@users.noreply.github.com>
2021-06-09 17:36:31 +0300

    js file added

---
+++ index.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll("button")[0].addEventListener("click", function(){
+
+document.addEventListener("click", function(){
+
+  //$("div .navbarResponsive").slideUp(400);
+  var x = document.getElementById("navbarResponsive");
+  x.classList.remove('show');
+});
+});

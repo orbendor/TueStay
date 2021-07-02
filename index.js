980ab91d49808d3486161632cc5087c3b0322fd9
orbendor <73276306+orbendor@users.noreply.github.com>
2021-07-02 12:09:45 +0300

    about sliding

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ $("button")[0].addEventListener("click", function(){
 
 document.addEventListener("click", function(){
 
-  //$("div .navbarResponsive").slideUp(400);
   var x = document.getElementById("navbarResponsive");
   x.classList.remove('show');
 });
@@ -14,4 +13,17 @@ document.addEventListener("click", function(){
 $('#about').click(function() {
   $('.ceo').slideToggle("slow");
 
+  if($('#aboutceo').hasClass('hiddenDiv')){
+  $('.hiddenDiv').addClass('hiddenDivOpen').removeClass('hiddenDiv');
+  }else{
+    $('.hiddenDivOpen').addClass('hiddenDiv').removeClass('hiddenDivOpen');
+  }
+});
+
+$('#navbarResponsive ul li a').on('click',function(event){
+   var $anchor = $(this);
+   $('html, body').animate({
+     scrollTop: $($anchor.attr('href')).offset().top + "px"
+   }, 1500);
+   event.preventDefault();
 });
